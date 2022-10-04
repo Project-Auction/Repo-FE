9f430848b8db89245d605a80894c7a08131b73a1
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2022-10-04 14:33:07 +0700

    Creating and handle -"Checkbox"

--- auction-fe/src/shared/components/UIElement/Table/Table.js
+++ auction-fe/src/shared/components/UIElement/Table/Table.js
@@ -15,63 +15,88 @@ const Table = (props) => {
     new Array(props.items.length).fill(false)
   );
 
+  /* To store items selected */
+  const [itemsSelected, setItemsSelected] = useState([]);
+
   const handleCheckedAll = (event) => {
+    if (event.target.checked) {
+      setItemsSelected(props.items);
+    } else {
+      setItemsSelected([]);
+    }
     const updateCheckedState = checkedState.fill(event.target.checked);
     setCheckedState(updateCheckedState.map((state) => state));
   };
 
-  const handleChange = (pos) => {
+  const handleChange = (item, pos, event) => {
     const updateCheckedState = checkedState.map((state, index) =>
       pos === index ? !state : state
     );
 
     setCheckedState(updateCheckedState);
+
+    setItemsSelected((prev) => [...prev, item]);
+
+    /* Remove elements when unchecked */
+    if (!event.target.checked) {
+      setItemsSelected((prev) => prev.filter((val) => val !== item));
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(itemsSelected);
   };
 
   return (
     <div className="table__container table-responsive">
-      <table className={classes}>
-        <thead className={classes}>
-          <tr>
-            {props.select && (
-              <th scope="col" className="selecting">
-                <CheckboxField
-                  fontSize={22}
-                  color="#000"
-                  checkedColor="#ff3366"
-                  onChange={(e) => handleCheckedAll(e)}
-                />
-              </th>
-            )}
-            {props.header.map((header, index) => (
-              <th scope="col" key={index}>
-                {header.field}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {props.items.map((item, index) => (
-            <tr key={index}>
+      <form>
+        <table className={classes}>
+          <thead className={classes}>
+            <tr>
               {props.select && (
-                <th>
+                <th scope="col" className="selecting">
                   <CheckboxField
                     fontSize={22}
                     color="#000"
                     checkedColor="#ff3366"
-                    onChange={() => handleChange(index)}
-                    checked={checkedState[index]}
+                    onChange={(e) => handleCheckedAll(e)}
                   />
                 </th>
               )}
-              <th scope="row">{item.codeProduct}</th>
-              <td>{item.name}</td>
-              <td>{item.headerTitle}</td>
-              <td>{item.initialPrice}</td>
+              {props.header.map((header, index) => (
+                <th scope="col" key={index}>
+                  {header.field}
+                </th>
+              ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {props.items.map((item, pos) => (
+              <tr key={pos}>
+                {props.select && (
+                  <th>
+                    <CheckboxField
+                      fontSize={22}
+                      color="#000"
+                      checkedColor="#ff3366"
+                      onChange={(e) => handleChange(item, pos, e)}
+                      checked={checkedState[pos]}
+                    />
+                  </th>
+                )}
+                <th scope="row">{item.codeProduct}</th>
+                <td>{item.name}</td>
+                <td>{item.headerTitle}</td>
+                <td>{item.initialPrice}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <button type="submit" onClick={handleSubmit}>
+          SUBMIT
+        </button>
+      </form>
     </div>
   );
 };
