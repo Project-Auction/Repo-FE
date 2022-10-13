9eb63a0ee7e1c629ae390f2d9da6552baca9821b
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2022-10-13 20:59:28 +0700

    Done Show Product

--- auction-fe/src/shared/components/UIElement/Card/CardProduct.js
+++ auction-fe/src/shared/components/UIElement/Card/CardProduct.js
@@ -14,75 +14,77 @@ function CardProduct(props) {
 
   const [days, hours, minutes, seconds] = useCountDown(TIME_IN_SYSTEM);
 
-  return (
-    <div className={`card__wrapper card__product ${props.className}`}>
-      <div className={`card__content ${props.contentClass}`}>
-        <div className="card__img-group">
-          <img
-            className={`card__content-img ${props.imageClass}`}
-            src={props.image}
-            alt={props.name}
-          />
+  if (days + hours + minutes + seconds > 0) {
+    return (
+      <div className={`card__wrapper card__product ${props.className}`}>
+        <div className={`card__content ${props.contentClass}`}>
+          <div className="card__img-group">
+            <img
+              className={`card__content-img ${props.imageClass}`}
+              src={props.image}
+              alt={props.name}
+            />
 
-          {props.headerTitle && (
-            <div className={`card__title-status ${props.headerClass}`}>
-              <h2>{props.headerTitle}</h2>
+            {props.headerTitle && (
+              <div className={`card__title-status ${props.headerClass}`}>
+                <h2>{props.headerTitle}</h2>
+              </div>
+            )}
+
+            <ButtonField className="card__btn-bid" primary>
+              SUBMIT A BID
+            </ButtonField>
+          </div>
+
+          <Link to={`${props.codeProduct}/detail`} className="info">
+            <span className="category">Vehicles</span>
+
+            <h3 className="product__name">BMW 5 Series GT Car</h3>
+
+            <div className="range__money">
+              <p className="price">
+                Start Price:
+                <span>$550.00</span>
+              </p>
+
+              <p className="price">
+                Current Price:
+                <span>$550.00</span>
+              </p>
             </div>
-          )}
+          </Link>
 
-          <ButtonField className="card__btn-bid" primary>
-            SUBMIT A BID
-          </ButtonField>
-        </div>
-
-        <Link to={`${props.codeProduct}/detail`} className="info">
-          <span className="category">Vehicles</span>
-
-          <h3 className="product__name">BMW 5 Series GT Car</h3>
-
-          <div className="range__money">
-            <p className="price">
-              Start Price:
-              <span>$550.00</span>
+          <div className="card__content-footer">
+            <p className="remaining__time-group">
+              {days}
+              <span>Days</span>
             </p>
 
-            <p className="price">
-              Current Price:
-              <span>$550.00</span>
+            <span className="dots">:</span>
+
+            <p className="remaining__time-group">
+              {hours}
+              <span>Hours</span>
+            </p>
+
+            <span className="dots">:</span>
+
+            <p className="remaining__time-group">
+              {minutes}
+              <span>Minutes</span>
+            </p>
+
+            <span className="dots">:</span>
+
+            <p className="remaining__time-group">
+              {seconds}
+              <span>Seconds</span>
             </p>
           </div>
-        </Link>
-
-        <div className="card__content-footer">
-          <p className="remaining__time-group">
-            {days}
-            <span>Days</span>
-          </p>
-
-          <span className="dots">:</span>
-
-          <p className="remaining__time-group">
-            {hours}
-            <span>Hours</span>
-          </p>
-
-          <span className="dots">:</span>
-
-          <p className="remaining__time-group">
-            {minutes}
-            <span>Minutes</span>
-          </p>
-
-          <span className="dots">:</span>
-
-          <p className="remaining__time-group">
-            {seconds}
-            <span>Seconds</span>
-          </p>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default CardProduct;
