b904d6f3413f53e648eb9d5cccfc3f58b57caf90
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2022-10-14 14:31:38 +0700

    Creating form "Update Account"

--- auction-fe/src/app/users/components/profile/EditProfile/EditProfile.js
+++ auction-fe/src/app/users/components/profile/EditProfile/EditProfile.js
@@ -1,16 +1,35 @@
+import { useForm } from "react-hook-form";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCamera } from "@fortawesome/free-solid-svg-icons";
+
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
 
 import "./EditProfile.css";
 import Image from "../../../../../shared/components/UIElement/Image";
 import ProfileUser from "../../../page/profile/ProfileUser";
 import CustomFormProvider from "../../../../../shared/components/FormElement/CustomFormProvider";
-import { useForm } from "react-hook-form";
 import { FormInput } from "../../../../../shared/components/FormElement/Input";
 import ButtonFiled from "../../../../../shared/components/FormElement/Button";
+import FormInputTime from "../../../../../shared/components/FormElement/InputTime";
 
 const EditProfile = (props) => {
-  const methods = useForm();
+  const formSchema = Yup.object().shape({
+    confirmPassword: Yup.string().oneOf(
+      [Yup.ref("password")],
+      "Password not matching"
+    ),
+  });
+
+  const methods = useForm({
+    mode: "onChange",
+    resolver: yupResolver(formSchema),
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   return (
     <ProfileUser>
@@ -32,84 +51,94 @@ const EditProfile = (props) => {
           </div>
 
           <CustomFormProvider {...methods}>
-            <form className="form__edit-profile-body-form">
+            <form
+              className="form__edit-profile-body-form"
+              onSubmit={methods.handleSubmit(onSubmit)}
+            >
               <div className="form__edit-profile-body-form__group">
                 <FormInput
-                  fieldName="fullName"
-                  control={methods.control}
                   isMui
+                  variant="outlined"
+                  fieldName="username"
+                  type="text"
+                  fullWidth
+                  onFocus={() => {}}
                   label="Full Name"
-                  onFocus={() => {}}
+                  noBorder
+                  className="mr-4"
                   requiredForm
+                  messageRequired="Username cannot be empty"
+                  minLengthForm={6}
+                  minLengthMessage="Username at least 6 characters"
                 />
-
                 <FormInput
-                  fieldName="email"
-                  control={methods.control}
                   isMui
-                  label="Email"
+                  fieldName="accountName"
+                  type="text"
+                  fullWidth
                   onFocus={() => {}}
+                  required
+                  label="Account Name"
                   requiredForm
+                  messageRequired="Account name cannot be empty"
+                  minLengthForm={6}
+                  minLengthMessage="Account name at least 6 characters"
                 />
               </div>
 
               <div className="form__edit-profile-body-form__group">
                 <FormInput
-                  fieldName="fullName"
-                  control={methods.control}
                   isMui
-                  label="Full Name"
+                  fieldName="email"
+                  type="email"
+                  fullWidth
                   onFocus={() => {}}
+                  className="mr-4"
+                  label="Email"
                   requiredForm
+                  messageRequired="Email cannot be empty"
+                  minLengthForm={6}
+                  minLengthMessage="Email at least be 6 characters"
+                  emailRequired
                 />
 
-                <FormInput
-                  fieldName="email"
-                  control={methods.control}
-                  isMui
-                  label="Email"
-                  onFocus={() => {}}
+                <FormInputTime
+                  fieldName="dateOfBirth"
+                  dataType="date_timer_picker"
+                  label="Date of birth"
                   requiredForm
+                  messageRequired="Date of birth cannot be empty"
                 />
               </div>
 
               <div className="form__edit-profile-body-form__group">
                 <FormInput
-                  fieldName="fullName"
-                  control={methods.control}
                   isMui
-                  label="Full Name"
+                  fieldName="phoneNumber"
+                  type="text"
+                  fullWidth
                   onFocus={() => {}}
+                  className="mr-4"
+                  label="Phone Number"
+                  format="phone_number"
                   requiredForm
+                  messageRequired="Phone number cannot be empty"
+                  minLengthForm={9}
+                  minLengthMessage="Phone number at least be 9 characters"
                 />
 
                 <FormInput
-                  fieldName="email"
-                  control={methods.control}
                   isMui
-                  label="Email"
+                  fieldName="identityNumber"
+                  type="text"
+                  fullWidth
                   onFocus={() => {}}
+                  label="Identity Number"
+                  format="identity_card"
                   requiredForm
-                />
-              </div>
-
-              <div className="form__edit-profile-body-form__group">
-                <FormInput
-                  fieldName="fullName"
-                  control={methods.control}
-                  isMui
-                  label="Full Name"
-                  onFocus={() => {}}
-                  requiredForm
-                />
-
-                <FormInput
-                  fieldName="email"
-                  control={methods.control}
-                  isMui
-                  label="Email"
-                  onFocus={() => {}}
-                  requiredForm
+                  messageRequired="Identity number cannot be empty"
+                  minLengthForm={9}
+                  minLengthMessage="Identity number at least be 9 characters"
                 />
               </div>
 
