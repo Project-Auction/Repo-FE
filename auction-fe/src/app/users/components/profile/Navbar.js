db3e15aa338cc259be072eb0c28f261f79289ed9
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2022-10-14 09:12:09 +0700

    Creating "Navbar Profile"

--- auction-fe/src/app/users/components/profile/Navbar.js
+++ auction-fe/src/app/users/components/profile/Navbar.js
@@ -1,14 +1,59 @@
 import "./Navbar.css";
 
+import { NavLink } from "react-router-dom";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBolt, faHouse, faReceipt } from "@fortawesome/free-solid-svg-icons";
+import { faEdit } from "@fortawesome/free-regular-svg-icons";
+
 import CardField from "../../../../shared/components/UIElement/Card/CardField";
+import AccountItem from "../../../../shared/components/UIElement/AccountItem";
 
 const Navbar = () => {
   return (
     <div className="navbar__profile-user-container">
-      <CardField>
+      <CardField className="navbar__profile-user-wrapper">
         <div className="navbar__profile-user__header">
-            
+          <AccountItem
+            src="https://demo.graygrids.com/themes/classigrids-demo/assets/images/dashboard/user-image.jpg"
+            userName="Steve Aldridge"
+            email="@username"
+          />
         </div>
+
+        <ul className="navbar__profile-user__body">
+          <li className="navbar__profile-user__item">
+            <NavLink end>
+              <FontAwesomeIcon icon={faHouse} className="icon" />
+              Dashboard
+              <div className="separate-hover"></div>
+            </NavLink>
+          </li>
+
+          <li className="navbar__profile-user__item">
+            <NavLink end to={`/${3}/edit`}>
+              <FontAwesomeIcon icon={faEdit} className="icon" />
+              Edit Profile
+              <div className="separate-hover"></div>
+            </NavLink>
+          </li>
+
+          <li className="navbar__profile-user__item">
+            <NavLink end to={`/${3}/myAds`}>
+              <FontAwesomeIcon icon={faBolt} className="icon" />
+              Dashboard
+              <div className="separate-hover"></div>
+            </NavLink>
+          </li>
+
+          <li className="navbar__profile-user__item">
+            <NavLink end to={`/${3}/invoices`}>
+              <FontAwesomeIcon icon={faReceipt} className="icon" />
+              Invoice
+              <div className="separate-hover"></div>
+            </NavLink>
+          </li>
+        </ul>
       </CardField>
     </div>
   );
--- auction-fe/src/app/users/page/profile/ProfileUser.js
+++ auction-fe/src/app/users/page/profile/ProfileUser.js
@@ -2,21 +2,29 @@ import { useParams } from "react-router";
 import "./ProfileUser.css";
 
 import { NavbarProfile } from "../../components/profile";
+import MainNavigation from "../../../../shared/components/UIElement/Navigation/MainNavigation";
+import HeaderBreadcrumbs from "../../../../shared/components/UIElement/HeaderBreadcrumbs/HeaderBreadcrumbs";
 
 const ProfileUser = (props) => {
   const userId = useParams().userId;
 
   return (
-    <div className="profile__user-container">
-      <div className="container">
-        <div className="row">
-          <div className="col-3">
-            <NavbarProfile />
+    <>
+      <MainNavigation />
+
+      <HeaderBreadcrumbs currentPage="Profile User" />
+
+      <div className="profile__user-container section">
+        <div className="container">
+          <div className="row">
+            <div className="col-3">
+              <NavbarProfile />
+            </div>
+            <div className="col-9"></div>
           </div>
-          <div className="col-9"></div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
