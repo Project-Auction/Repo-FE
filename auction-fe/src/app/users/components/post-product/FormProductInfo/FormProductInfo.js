6cc3134d5628e002dcce15cfcb89d60ff80e63c3
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2022-10-15 22:24:13 +0700

    CSS and order file

--- auction-fe/src/app/users/components/post-product/FormProductInfo/FormProductInfo.js
+++ auction-fe/src/app/users/components/post-product/FormProductInfo/FormProductInfo.js
@@ -1,13 +1,11 @@
-import FormStep from "../FormStep/FormStep";
 import "./FormProductInfo.css";
 
 const FormProductInfo = (props) => {
+
   return (
-    <FormStep>
-      <div className="form__input-container">
-        <h1>FormProductInfo</h1>
-      </div>
-    </FormStep>
+    <div className="form__input-container">
+      <h1>FormProductInfo</h1>
+    </div>
   );
 };
 
--- auction-fe/src/app/users/page/post-product/PostProduct.js
+++ auction-fe/src/app/users/page/post-product/PostProduct.js
@@ -1,11 +1,27 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+import "./PostProduct.css";
 import FormProductDetail from "../../components/post-product/FormProductDetail/FormProductDetail";
 import FormProductInfo from "../../components/post-product/FormProductInfo/FormProductInfo";
 import FormUserInfo from "../../components/post-product/FormUserInfo/FormUserInfo";
-import "./PostProduct.css";
+import ButtonFiled from "../../../../shared/components/FormElement/Button";
+import DashboardUser from "../user-dashboard/DashboardUser";
+import CustomFormProvider from "../../../../shared/components/FormElement/CustomFormProvider";
+import HeaderStep from "../../components/post-product/HeaderStep";
 
 const PostProduct = (props) => {
+  const methods = useForm();
+
   const [steps, setSteps] = useState(0);
+
+  const handleNextStep = () => {
+    setSteps(steps + 1);
+  };
+
+  const handlePrevStep = () => {
+    setSteps(steps - 1);
+  };
 
   const conditionalComponents = () => {
     switch (steps) {
@@ -23,7 +39,63 @@ const PostProduct = (props) => {
     }
   };
 
-  return <>{conditionalComponents()}</>;
+  return (
+    <>
+      <DashboardUser currentPage="Post Product">
+        <div className="form__step-container">
+          <header className="form__step-header">
+            <h3 className="title">Post Product</h3>
+          </header>
+
+          <CustomFormProvider {...methods}>
+            <div className="form__step-form-body">
+              <div className="form__step-form-header">
+                <HeaderStep
+                  isActive={steps + 1 === 1}
+                  numberStep="01"
+                  subTitleStep={"Ad Information"}
+                />
+                <HeaderStep
+                  isActive={steps + 1 === 2}
+                  numberStep="02"
+                  subTitleStep={"Ad details"}
+                />
+                <HeaderStep
+                  isActive={steps + 1 === 3}
+                  numberStep="03"
+                  subTitleStep={"User Information"}
+                />
+              </div>
+              <form className="form__step-form">{conditionalComponents()}</form>
+            </div>
+
+            <div className="form__step-form-footer">
+              {steps > 0 && (
+                <ButtonFiled
+                  type="button"
+                  onClick={handlePrevStep}
+                  className="btn__redirect-form prev"
+                >
+                  Prev Step
+                </ButtonFiled>
+              )}
+
+              {steps < 2 && (
+                <ButtonFiled
+                  type="button"
+                  onClick={handleNextStep}
+                  className="btn__redirect-form next"
+                  dark
+                >
+                  Next Step
+                </ButtonFiled>
+              )}
+            </div>
+          </CustomFormProvider>
+        </div>
+      </DashboardUser>
+    </>
+  );
 };
 
 export default PostProduct;
